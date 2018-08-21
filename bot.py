e8f61353ad2406acd90056057f5938764278a07f
trinhvv <vanvuong.trinh@gmail.com>
2018-08-21 17:04:58 +0200

    Commit time: 2018-08-21 17:04:58

--- bot.py
+++ bot.py
@@ -6,10 +6,6 @@ from telegram.ext import Updater,CommandHandler,MessageHandler,Filters
 
 from binance.client import Client
 
-import logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
-logger=logging.getLogger(__name__)
-
 import infolib,tradelib,indexlib,misclib,btclib
 
 TOKEN=os.environ['TELEGRAM_TOKEN']
