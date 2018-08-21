3fbc704b2c47ab4361a1cbb7ccc8a8ae276efd98
trinhvv <vanvuong.trinh@gmail.com>
2018-08-21 03:19:46 +0200

    Commit time: 2018-08-21 03:19:46

--- btclib.py
+++ btclib.py
@@ -1,8 +1,6 @@
 import urllib,json
 
 def btc_alarm():
-    BUY_TOL=20.
-    VOL_DIFF=-100.
     url = "https://api.bitfinex.com/v1/trades/btcusd?limit_trades=500"
     trades=json.loads(urllib.urlopen(url).read())
     past_time=trades[0]['timestamp']-5*60  
@@ -11,8 +9,10 @@ def btc_alarm():
     total_buy=int(sum(buy_volume))
     total_sell=int(sum(sell_volume))
     vol_diff=total_buy-total_sell
-    msg='Bitfinex BTCUSD (Last 5 mins):\n- Buy volume: '+"{:,}".format(total_buy)+'\n- Sell volume: '+"{:,}".format(total_sell)+')\n- Difference volume: '+"{:,}".format(vol_diff)
-    if total_buy<=BUY_TOL and vol_diff<=VOL_DIFF:
+    msg='Bitfinex BTCUSD (Last 5 mins):\n- Buy volume: '+"{:,}".format(total_buy)+'\n- Sell volume: '+"{:,}".format(total_sell)+'\n- Difference volume: '+"{:,}".format(vol_diff)
+    if total_buy<=20 and vol_diff<=-100:
+        alarm=True
+    elif vol_diff>=100:
         alarm=True
     else:
         alarm=False
